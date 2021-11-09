dc408be8c21d314b21662e9a5e00b65327a0b18a
Mustafa Hakan Orskaya <mhorskaya@hotmail.com>
2021-11-09 23:46:02 +0300

    initial commit

---
+++ Rocket.Tests/PlatformTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Rocket.Tests
+{
+    public class PlatformTests
+    {
+        [Theory]
+        [InlineData(10, 0, -1, 0)]
+        [InlineData(0, 10, 0, -1)]
+        [InlineData(0, 10, 5, 5)]
+        [InlineData(10, 0, 5, 5)]
+        public void Platform_Creates_Correctly(int x, int y, int top, int left)
+        {
+            Assert.Throws<Exception>(() => new Platform(x, y, top, left));
+        }
+
+        [Theory]
+        [InlineData(10, 10, 5, 5, new[] { "5,5,0", "16,15,1" })]
+        [InlineData(10, 10, 5, 5, new[] { "7,7,0", "8,8,2" })]
+        [InlineData(10, 10, 5, 5, new[] { "7,7,0", "8,8,2", "1,1,1" })]
+        [InlineData(10, 10, 5, 5, new[] { "7,7,0", "10,10,0", "7,7,0", "8,8,2" })]
+        public void Platform_Results_Correctly(int x, int y, int top, int left, string[] checks)
+        {
+            var platform = new Platform(x, y, top, left);
+            foreach (var check in checks)
+            {
+                var parts = check.Split(",").Select(int.Parse).ToArray();
+                var result = platform.Check(parts[0], parts[1]);
+                Assert.Equal((int)result, parts[2]); // enum Result { OkForLanding = 0, OutOfPlatform = 1, Clash = 2 }
+            }
+        }
+    }
+}
---
+++ Rocket/Platform.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Rocket
+{
+    public enum Result { OkForLanding = 0, OutOfPlatform = 1, Clash = 2 }
+
+    public class Platform
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public (int X, int Y)? Previous { get; set; }
+
+        public Platform(int x, int y, int top, int left)
+        {
+            if (x < 1 || y < 1)
+                throw new Exception("Platform must have at least (1, 1) size.");
+
+            if (top < 0 || left < 0)
+                throw new Exception("Platform must be inside landing area.");
+
+            (X, Y, Top, Left) = (x, y, top, left);
+        }
+
+        public Result Check(int x, int y)
+        {
+            if (x < Top || x > Top + X || y < Left || y > Left + Y)
+            {
+                Previous = (x, y);
+                return Result.OutOfPlatform;
+            }
+
+            if (Previous != null && Previous?.X - 1 <= x && x <= Previous?.X + 1 && Previous?.Y - 1 <= y && y <= Previous?.Y + 1)
+            {
+                Previous = (x, y);
+                return Result.Clash;
+            }
+
+            Previous = (x, y);
+            return Result.OkForLanding;
+        }
+    }
+}
